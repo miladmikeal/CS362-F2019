3ddf34ba723284304ee14a6781056974ec0d1416
Milad Mikeal <miladmikeal@gmail.com>
2019-10-12 20:03:18 -0700

    Refactor dominion.c by placing 5 cards into their own funcitons

---
+++ projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/dominion.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/dominion.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/interface.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/interface.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/playdom.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/playdom.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/player.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/player.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/rngs.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/rngs.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/rt.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/rt.c.o"
+  "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/testdrawcard.c" "/Users/miladmikeal/CS/cs362/week1/CS362-F2019/projects/mikealm/dominion/cmake-build-debug/CMakeFiles/dominion.dir/testdrawcard.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
